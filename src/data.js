5e28c507652e07d69f510904acc5a3a495730706
FlorentRicciardi <florent.ricciardi@sogeti.com>
2021-10-02 11:34:38 +0200

    first commit

---
+++ src/data.js
@@ -0,0 +1,70 @@
+export const sliderItems = [
+
+    {
+      id: 1,
+      img: "http://f5-footfive.fr/wp-content/themes/gl_theme_2/library/images/joueur-foot-home.png",
+      title: "AUTUMN COLLECTION",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fcf1ed",
+    },
+    {
+      id: 2,
+      img: "http://pngimg.com/uploads/fitness/fitness_PNG150.png",
+      title: "SUMMER LOVE",
+      desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+      bg: "fbf0f4",
+    },
+  ];
+
+  export const categories = [
+    {
+      id: 1,
+      img: "https://www.fondationeczema.org/sites/default/files/2020-07/football-760x614px.jpg",
+      title: "FOOTBALL",
+    },
+    {
+      id: 2,
+      img: "https://fac.img.pmdstatic.net/fit/http.3A.2F.2Fprd2-bone-image.2Es3-website-eu-west-1.2Eamazonaws.2Ecom.2Ffac.2F2020.2F08.2F21.2Fe5c39a5f-5ede-4f2d-a916-045dc6b36489.2Ejpeg/850x478/quality/90/crop-from/center/running-la-routine-ideale-pour-bruler-plus-de-calories.jpeg",
+      title: "RUNNING",
+    },
+    {
+      id: 3,
+      img: "https://www.sharkfitness.ch/img/wecke-den-shark-in-dir_5f881b5ae2279.jpg",
+      title: "FITNESS",
+    },
+  ];
+
+  export const popularProducts = [
+    {
+      id:1,
+      img:"https://contents.mediadecathlon.com/p2087798/k$04d7ce0bed51a7b3a8a894be0e62565c",
+    },
+    {
+      id:2,
+      img:"https://static.nike.com/a/images/t_default/lgqmewo2wiujgng7naas/chaussure-tanjun-pour-MkTmejeq.png",
+    },
+    {
+      id:3,
+      img:"https://media.intersport.fr/is/image/intersportfr/2103-tradeLP-maillotFFF-producthero-3@1x?wid=500&hei=450&fmt=png-alpha",
+    },
+    {
+      id:4,
+      img:"https://www.clickandsport.fr/15491-home_default/w-nike-air-zoom-pegasus-38.jpg",
+    },
+    {
+      id:5,
+      img:"https://static.nike.com/a/images/t_default/wcg4n1knnei3yqbr4s23/sac-de-sport-de-training-brasilia-vdVDMs.png",
+    },
+    {
+      id:6,
+      img:"https://i.pinimg.com/originals/a3/82/51/a38251d0864f19ec4b7e080bac6197ac.png",
+    },
+    {
+      id:7,
+      img:"https://static.nike.com/a/images/t_default/b52a49f1-c2d4-467b-ad97-479d33aca121/veste-de-survetement-de-football-tisse-dri-fit-academy-pour-mXrVD7.png",
+    },
+    {
+      id:8,
+      img:"https://static.nike.com/a/images/t_default/b58c9ed2-2a8c-428a-b504-8f156049fcce/veste-de-football-pologne-pour-6Xb7QW.png",
+    },
+  ]
